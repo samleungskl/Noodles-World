028a2f396e8ddcd1f605e890b16805a98c4a5810
samleung <samleung@samleungs-Virtual-Machine.local>
2022-08-25 20:56:17 -0700

    initial commit

---
+++ public/scripts/dayOfWeek.js
@@ -0,0 +1,29 @@
+const numberToDayOfWeek = function (number) {
+    let day = '';
+    switch (number) {
+        case 1:
+            day = "Mon";
+            break;
+        case 2:
+            day = "Tue";
+            break;
+        case 3:
+            day = "Wed";
+            break;
+        case 4:
+            day = "Thu";
+            break;
+        case 5:
+            day = "Fri";
+            break;
+        case 6:
+            day = "Sat";
+            break;
+        case 7:
+            day = "Sun";
+    }
+
+    return (day)
+}
+
+module.exports.numberToDayOfWeek = numberToDayOfWeek;
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const dayOfweek = require('./public/scripts/dayOfweek.js');
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+// use res.render to load up an ejs view file
+
+function getItems() {
+  return axios.get('https://noodles-world-api.herokuapp.com/items');
+}
+
+function getHours() {
+  return axios.get('https://noodles-world-api.herokuapp.com/hours');
+}
+
+function getResturants() {
+  return axios.get('https://noodles-world-api.herokuapp.com/resturants');
+}
+
+// index page
+app.get('/', function (req, res) {
+  Promise.all([getItems(), getHours(), getResturants()])
+    .then(function (results) {
+      const items = results[0];
+      const hours = results[1];
+      const resturants = results[2];
+      res.render("pages/index", {
+        items: items.data,
+        hours: hours.data,
+        resturants: resturants.data,
+        dayOfweekFn: dayOfweek
+      });
+    })
+    .catch(function (error) {
+      // handle errors appropriately
+      res.render("error", { error });
+    });
+})
+
+// about page
+app.get('/about', function (req, res) {
+  res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
